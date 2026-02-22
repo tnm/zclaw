d48eee0b496f56cf95be17996c35331907126525
tnm <t@ted.io>
2026-02-21 16:01:12 -0800

    release: bump version to 2.1.1

--- main/ota.c
+++ main/ota.c
@@ -8,7 +8,7 @@ static bool s_pending_verify = false;
 
 // Current firmware version (set at compile time)
 #ifndef ZCLAW_VERSION
-#define ZCLAW_VERSION "2.1.0"
+#define ZCLAW_VERSION "2.1.1"
 #endif
 
 esp_err_t ota_init(void)
